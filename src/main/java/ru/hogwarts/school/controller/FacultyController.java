edb6d304be4c50f0710b81d85acf17544ac25a35
lalcalutik <themorozushka@gmail.com>
2023-09-29 08:38:20 +0600

    hw is done

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,54 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.dto.FacultyDtoIn;
+import ru.hogwarts.school.dto.FacultyDtoOut;
+import ru.hogwarts.school.dto.StudentDtoOut;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+
+    @PostMapping
+    public FacultyDtoOut createFaculty(@RequestBody FacultyDtoIn facultyDtoIn) {
+        return facultyService.createFaculty(facultyDtoIn);
+    }
+
+    @GetMapping
+    public Collection<FacultyDtoOut> getFaculties(@RequestParam(required = false) String colorOrName) {
+        if (colorOrName != null && !colorOrName.isBlank()) {
+            return facultyService.getFacultiesByColorOrName(colorOrName);
+        }
+        return facultyService.getAllFaculties();
+    }
+
+    @GetMapping("{id}")
+    public FacultyDtoOut getFaculty(@PathVariable Long id) {
+        return facultyService.getFaculty(id);
+    }
+
+    @GetMapping("{id}/students")
+    public Collection<StudentDtoOut> getStudents(@PathVariable Long id) {
+        return facultyService.getStudents(id);
+    }
+
+    @PutMapping("{id}")
+    public FacultyDtoOut editFaculty(@PathVariable Long id, @RequestBody FacultyDtoIn facultyDtoIn) {
+        return facultyService.editFaculty(id, facultyDtoIn);
+    }
+
+    @DeleteMapping("{id}")
+    public FacultyDtoOut removeFaculty(@PathVariable Long id) {
+        return facultyService.removeFaculty(id);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/mapper/FacultyMapper.java
@@ -0,0 +1,25 @@
+package ru.hogwarts.school.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.hogwarts.school.dto.FacultyDtoIn;
+import ru.hogwarts.school.dto.FacultyDtoOut;
+import ru.hogwarts.school.entity.Faculty;
+
+@Component
+public class FacultyMapper {
+
+    public FacultyDtoOut toDto(Faculty faculty) {
+        FacultyDtoOut facultyDtoOut = new FacultyDtoOut();
+        facultyDtoOut.setId(faculty.getId());
+        facultyDtoOut.setName(faculty.getName());
+        facultyDtoOut.setColor(faculty.getColor());
+        return facultyDtoOut;
+    }
+
+    public Faculty toEntity(FacultyDtoIn facultyDtoIn) {
+        Faculty faculty = new Faculty();
+        faculty.setName(facultyDtoIn.getName());
+        faculty.setColor(facultyDtoIn.getColor());
+        return faculty;
+    }
+}
